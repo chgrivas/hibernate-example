1b33edfced1a7d9b90a6a0f409f3948862ce966a
chgri <chgrivas@gmail.com>
2015-04-23 22:35:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Description
+This project is an example using hibernate working as an archetype for future projects.
+
+# TODO
+* Setup mysql
+* Undestand mapping
+
+# Contact info
+Chris Grivas <chgrivas7@gmail.com>
---
+++ src/main/java/com/projects/example/hibernate/base/Main.java
@@ -0,0 +1,33 @@
+package com.projects.example.hibernate.base;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Department department = new Department("java");
+        session.save(department);
+
+        session.save(new Employee("Jakab Gipsz", department));
+        session.save(new Employee("Captain Nemo", department));
+
+        session.getTransaction().commit();
+
+        Query q = session.createQuery("From Employee ");
+
+        List<Employee> resultList = q.list();
+        System.out.println("num of employess:" + resultList.size());
+        for (Employee next : resultList) {
+            System.out.println("next employee: " + next);
+        }
+
+    }
+
+}
